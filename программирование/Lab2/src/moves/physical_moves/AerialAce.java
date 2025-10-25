db1a6323a6ee60c495f1295de3cf2021d555086e
Екатерина Шинкарева <163672402+kifmu@users.noreply.github.com>
2025-10-25 12:14:46 +0300

    Add files via upload

---
+++ программирование/Lab2/src/moves/physical_moves/AerialAce.java
@@ -0,0 +1,19 @@
+package physical_moves;
+
+import ru.ifmo.se.pokemon.*;
+
+public class AerialAce extends PhysicalMove{
+    public AerialAce() {
+        super(Type.FLYING,60,Double.POSITIVE_INFINITY);
+    }
+    @Override
+    protected boolean checkAccuracy(Pokemon att, Pokemon def) {
+        return true;
+    }
+    
+    @Override
+     protected String describe() {
+        return "использует Aerial Ace";
+     }
+
+}
