1925aac4f883b9944c3c815d9ce1ecfff130041e
Екатерина Шинкарева <163672402+kifmu@users.noreply.github.com>
2025-10-25 12:15:57 +0300

    Add files via upload

---
+++ программирование/Lab2/src/moves/status_move/WorkUp.java
@@ -0,0 +1,20 @@
+package status_move;
+
+import ru.ifmo.se.pokemon.Pokemon;
+import ru.ifmo.se.pokemon.*;
+
+public class WorkUp extends StatusMove{
+    public WorkUp() {
+    }
+    int isUsed = 0;
+    public void applySelfEffects(Pokemon pokemon) {
+        if (isUsed <= 6) {
+            pokemon.setMod(Stat.ATTACK, 1);
+            pokemon.setMod(Stat.SPECIAL_ATTACK, 1);
+        }
+    }
+    @Override
+    protected String describe() {
+        return "использует Work Up";
+    }
+}
